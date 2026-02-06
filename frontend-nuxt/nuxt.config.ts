7191e6bb2bb03ba81224de0c016e8047f6d8567e
ZheerBarzan <zheerbarzan85@outlook.com>
2026-02-06 16:23:46 +0300

    fixing the fornt-end deployment

--- frontend-nuxt/nuxt.config.ts
+++ frontend-nuxt/nuxt.config.ts
@@ -2,10 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false, // Disable Server Side Rendering (makes it a purely static SPA)
+  ssr: false,
   runtimeConfig: {
     public: {
-      apiBase: 'https://qr-backend.onrender.com' // PASTE YOUR BACKEND URL HERE
+      apiBase: 'https://qrcodism.onrender.com'
     }
   }
 })
